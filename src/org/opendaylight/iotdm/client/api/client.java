92cd34a9043a95b348de04c7cc8b009b1064e4fd
vishleshpatel <vishlesh.patel@nyu.edu>
2017-02-11 16:08:05 -0800

    Committing Client Interface under API dir

--- src/org/opendaylight/iotdm/client/api/client.java
+++ src/org/opendaylight/iotdm/client/api/client.java
@@ -3,9 +3,6 @@ package org.opendaylight.iotdm.client.api;
 import org.opendaylight.iotdm.client.Request;
 import org.opendaylight.iotdm.client.Response;
 
-/**
- * Created by wenxshi on 6/2/15.
- */
 public interface Client {
     /**
      * Start client
