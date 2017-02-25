944ee33b510e0f94a436c5a7bb3c30be81630e48
Sameer Azad <sazad@cisco.com>
2017-02-25 17:40:57 -0500

    Test Commit for Sameer

--- src/org/opendaylight/iotdm/client/implementation/mqtt.java
+++ src/org/opendaylight/iotdm/client/implementation/mqtt.java
@@ -27,6 +27,8 @@ public class MQTT implements Client {
     String clientId = "JavaSample";
     MemoryPersistence persistence = new MemoryPersistence();
 
+    // Sameer test commit
+
     @Override
     public void start() {
         // test comment
